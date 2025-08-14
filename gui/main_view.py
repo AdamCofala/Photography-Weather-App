b53b31612bde5dc60836f6caca50d17d923fba5e
Adam C <9037852u@lo3otwock.eu>
2025-08-14 20:39:17 +0200

    Folium added

--- gui/main_view.py
+++ gui/main_view.py
@@ -1,14 +1,37 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+import folium
+import io
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import pyqtSignal
 
 
 class MainView(QWidget):
     def __init__(self):
         super().__init__()
 
-        layout = QVBoxLayout()
+        layout = QVBoxLayout(self)
 
-        # Na razie placeholder
-        self.label = QLabel("Tutaj będzie mapa lub statystyki")
-        layout.addWidget(self.label)
+        # Widok przeglądarki
+        self.web_view = QWebEngineView()
+        layout.addWidget(self.web_view)
 
-        self.setLayout(layout)
+        # Na start pokazujemy mapę
+        self.show_map()
+
+    def show_map(self, lat=52.2297, lon=21.0122, zoom=6):
+        """Generuje i wyświetla mapę w podanych współrzędnych"""
+        # Tworzymy mapę w folium
+
+        m = folium.Map(location=[lat, lon], zoom_start=zoom)
+
+        # Dodaj obsługę kliknięcia prawym przyciskiem (dodanie markera)
+        m.add_child(folium.LatLngPopup())  # Kliknięcie pokaże popup z lat/lon
+
+        # Konwersja do HTML w pamięci
+        data = io.BytesIO()
+        m.save(data, close_file=False)
+
+        # Wyświetlenie mapy w QWebEngineView
+        self.web_view.setHtml(data.getvalue().decode())
+
+
